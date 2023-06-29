1278e48c6b894810a0392da344535e76327fd325
Sumit Nagra <121888119+sumitnagra@users.noreply.github.com>
2023-06-29 09:34:12 +0530

    fix voice loading issue

--- src/text to speech/speech.js
+++ src/text to speech/speech.js
@@ -1,65 +1,86 @@
 import './speech.css'
 import { useState, useEffect } from 'react';
-const Speech = () => {
-    const [speechText, setText] = useState('')
-    const [voices, setVoices] = useState([])
-    const [selectedVoice, setSelectedVoice] = useState()
 
+const Speech = () => {
+    const [speechText, setText] = useState('');
+    const [voices, setVoices] = useState([]);
+    const [selectedVoice, setSelectedVoice] = useState();
+    const [voicesLoaded, setVoicesLoaded] = useState(false);
 
     useEffect(() => {
-        window.speechSynthesis.onvoiceschanged = () => {
-            setVoices(window.speechSynthesis.getVoices())
-           window.speechSynthesis.cancel();
-        }
-    }, [])
+        getVoices();
+        window.speechSynthesis.onvoiceschanged = getVoices;
+    }, []);
+
+    const getVoices = () => {
+        setVoices(window.speechSynthesis.getVoices());
+        setVoicesLoaded(true);
+    };
 
     const handleText = (e) => {
-        setText(e.target.value)
-    }
+        setText(e.target.value);
+    };
 
     const voiceSelect = (e) => {
-              const selectedVoiceIndex = e.target.value;
-            const selectedVoice = voices[selectedVoiceIndex];
-              setSelectedVoice(selectedVoice);
-          };
-          
-    
-    const startSpeeking = () => {
-        const speech = new SpeechSynthesisUtterance()
+        const selectedVoiceIndex = e.target.value;
+        const selectedVoice = voices[selectedVoiceIndex];
+        setSelectedVoice(selectedVoice);
+    };
+
+    const startSpeaking = () => {
+        const speech = new SpeechSynthesisUtterance();
         speech.text = speechText;
-        speech.voice=selectedVoice;
+        speech.voice = selectedVoice;
         if (window.speechSynthesis.paused) {
             window.speechSynthesis.resume();
-        }
-        else {
+        } else {
             speech.text = speechText;
-            speech.voice=selectedVoice;
-           window.speechSynthesis.speak(speech);
+            speech.voice = selectedVoice;
+            window.speechSynthesis.speak(speech);
         }
-    }
+    };
 
-    const pauseSpeech=()=> {
+    const pauseSpeech = () => {
         if (window.speechSynthesis.speaking && !window.speechSynthesis.paused) {
-          speechSynthesis.pause();
+            window.speechSynthesis.pause();
         }
-      }
-    return (<>
-        <div className='containerDiv'>
-            <div className="hero">
-                <h1>Text To Speech<span> Converter</span> </h1>
-                <textarea placeholder="Enter Text Here" value={speechText} onChange={handleText}></textarea>
-                <div className="drop">
-                    <select onChange={voiceSelect}>
-                        {
-                            voices.map((voice, i) => <option key={i} value={i}>{voice.name}</option>)
-                        }
-                    </select>
-                    <botton className="listen" onClick={startSpeeking}> Listen</botton>
-                    <botton className="stop" onClick={pauseSpeech}> Stop</botton>
+    };
+
+    return (
+        <>
+            <div className="containerDiv">
+                <div className="hero">
+                    <h1>
+                        Text To Speech<span> Converter</span>{' '}
+                    </h1>
+                    <textarea
+                        placeholder="Enter Text Here"
+                        value={speechText}
+                        onChange={handleText}
+                    ></textarea>
+                    <div className="drop">
+                        {voicesLoaded ? (
+                            <select onChange={voiceSelect}>
+                                {voices.map((voice, i) => (
+                                    <option key={i} value={i}>
+                                        {voice.name}
+                                    </option>
+                                ))}
+                            </select>
+                        ) : (
+                            <p>Loading voices...</p>
+                        )}
+                        <button className="listen" onClick={startSpeaking}>
+                            Listen
+                        </button>
+                        <button className="stop" onClick={pauseSpeech}>
+                            Stop
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>)
-}
+        </>
+    );
+};
 
 export default Speech;
