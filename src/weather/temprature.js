82cf9bbe8ab5226a4bf055ed4406b25e09979a74
Sumit Nagra <121888119+sumitnagra@users.noreply.github.com>
2023-08-15 12:51:55 +0530

    graph completed

--- src/weather/temprature.js
+++ src/weather/temprature.js
@@ -11,15 +11,61 @@ const Temprature = (props) => {
         <div className='graph'><div>
           <i className="fa-solid fa-cloud fa-2xl" style={{ "color": "darkgray" }} ></i>
         </div>
-        <svg width="100%" height="60%">
-          <circle cx="5" cy="5" stroke="red" r="5"/>
-        <path d="M0  0 L100 50" stroke="black"/>
+          <svg width="100%" height="60%">
+            <defs>
 
-</svg>
+              <linearGradient id="fadeGradient" x1="0%" y1="0%" x2="70%" y2="0%">
+                <stop offset="0%" stop-color="white" stop-opacity="1" />
+                <stop offset="100%" stop-color="white" stop-opacity="0" />
+              </linearGradient>
+            </defs>
+
+            <path d="M20 70 C 60 100, 130 100, 200 50" />
+
+            <path d="M20 70 C 60 100, 130 100, 200 50" stroke="url(#fadeGradient)" strokeWidth="8" />
+            <circle cx="95" cy="90" r="4"
+              stroke="darkorange" stroke-width="4" fill="darkorange" />
+          </svg>
           <h4>20°</h4><p>Morning</p></div>
-        <div className='graph'><div></div><div></div></div>
-        <div className='graph'><div></div><div></div></div>
-        <div className='graph'><div></div><div></div></div>
+        <div className='graph'><div>
+          <i className="fa-solid fa-sun fa-2xl" style={{ "color": "darkorange" }} ></i>
+        </div>
+          <svg width="100%" height="60%">
+            <circle cx="95" cy="29" r="4"
+              stroke="darkorange" stroke-width="4" fill="darkorange" />
+
+            <path d="M0 54 C 70 20, 140 20, 200 54" />
+          </svg>
+          <h4>34°</h4><p>Afternoon</p></div>
+        <div className='graph'><div>
+          <i className="fa-solid fa-cloud-sun fa-2xl" style={{ "color": "darkgray" }} ></i>
+        </div>
+          <svg width="100%" height="60%">
+            <circle cx="95" cy="82" r="4"
+              stroke="darkorange" stroke-width="4" fill="darkorange" />
+            <path d="M0 50 C 70 100, 140 85, 200 50" />
+
+          </svg>
+          <h4>20°</h4><p>Evening</p></div>
+        <div className='graph'><div>
+          <i className="fa-solid fa-moon fa-2xl" style={{ "color": "black" }} ></i>
+        </div>
+          <svg width="100%" height="60%">
+
+
+            <defs>
+              <linearGradient id="fadeGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" stop-color="white" stop-opacity="0" />
+                <stop offset="100%" stop-color="white" stop-opacity="1" />
+              </linearGradient>
+            </defs>
+            <path d="M0 54 C 70 20, 120 20, 180 40" />
+            <path d="M0 54 C 70 20, 120 20, 180 40" stroke="url(#fadeGradient)" strokeWidth="5" />
+            <circle cx="95" cy="27" r="4"
+              stroke="darkorange" stroke-width="4" fill="darkorange" />
+          </svg>
+          <h4>28°</h4><p>Night</p></div>
+
         <div className='tomorrow'></div>
       </div>
     </div>
