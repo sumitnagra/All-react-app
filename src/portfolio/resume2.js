39408ef26af32c1d527761f4b861a64169641060
Sumit Nagra <121888119+sumitnagra@users.noreply.github.com>
2023-08-30 21:59:46 +0530

    creating neew resume

--- src/portfolio/resume2.js
+++ src/portfolio/resume2.js
@@ -1,6 +1,10 @@
+import './resume2.css'
 const Resume2=()=>{
     return(<>
-    <h2>hwkki i</h2>
+    <div className="resume">
+        <h2>About</h2>
+        <p>i am a passonate froent web developer lorem i am very perticular about my shedule and always manage my time efficiently </p>
+    </div>
     </>)
 }
 export default Resume2
